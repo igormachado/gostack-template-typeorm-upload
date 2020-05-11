b0333348b2e01fea7090f2712f11de7a18d2239b
igormachado <rf59imft@gmail.com>
2020-05-11 12:16:48 -0300

    updated

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
-class DeleteTransactionService {
-  public async execute(): Promise<void> {
+import Transaction from '../models/Transaction';
+
+import AppError from '../errors/AppError';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
+
+class ImportTransactionsService {
+  async execute(id: string): Promise<void> {
     // TODO
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exists.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
-export default DeleteTransactionService;
+export default ImportTransactionsService;
